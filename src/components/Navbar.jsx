edd9c7e9a07f7e03ce6001263dbbbd6e5cf4d7ce
touraydev <t.cheikhou@yahoo.com>
2024-08-19 00:20:23 +0200

    modification6

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ const Navbar = () => {
   return (
     <div className="navbar fixed top-0 left-0 w-full p-4 bg-transparent flex items-center justify-between z-50">
       <div className="navbar-start">
-        <a className="navbar-item text-white font-bold">Portfolio </a>
+        <a  href='/' className="navbar-item text-white font-bold">Portfolio </a>
       </div>
       <div className="navbar-center space-x-4">
         <Link
